bbfdf2007b0353eae31d44556ca37fbd5e590640
CedricGAUTHIER <cedric.gauthier430@orange.fr>
2021-01-20 18:52:41 +0100

    adding content for about - before is WIP

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,10 +3,45 @@ import './style.scss';
 
 
 const About=() => {
-    
     return (
-        <div className="about">
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+        <div className="content">
+            <h1 className="content-about-title">Qui suis-je ?</h1>
+            <p>
+                C’est un peu mégalo de commencer comme cela.    
+            </p>
+            <p>
+                Un petit point rapide sur qui je suis :    
+            </p>
+            <hr></hr>
+            <p>
+                <strong>Cédric GAUTHIER</strong>, j’ai 46 ans et je vis dans un très charmant village de moyenne montagne en Savoie (au centre du triangle Chamonix, Annecy, Albertville).
+                Pour finir la carte postale, je dirais que les paysages sont magnifiques et que c’est encore plus beau l’hiver même si la neige peut-être très lourde.
+                <strong>Mais ce qui vous intéresse c’est plutôt la partie développeur, non ?</strong>
+            </p>
+            <hr></hr>
+            <p>
+                J’ai quitté mon ancien job en Mars 2020 (juste avant le confinement)  pour débuter une formation de 5 mois.
+                Il m’arrivait de “coder” en VBA pour Excel pour le boulot et j’aimais ça mais ce n’était qu’un début. La mentalité dans l’industrie (et peut-être plus encore dans la vallée de l’Arve) ne me convenait plus. Courir après la quantité plutôt que la qualité tout en vantant les bienfaits de la qualité me semblait de plus en plus compliqué.
+                Je n’avais pas envie de finir ma vie professionnelle dans ce domaine. Pour en savoir plus sur “ l’avant développeur ” voir la rubrique <em>Avant d’être développeur</em>
+            </p>
+            <p>
+                Alors, j’ai alors tenté le coup : reconversion pro (c’était un temps où la covid-19 n’existait pas encore) pour devenir développeur: voir la rubrique <em>Ma formation de développeur</em> pour plus de détails.
+            </p>
+            <p>
+                Ce qui me plaît dans le code c’est la possibilité de pouvoir automatiser des tâches quotidiennes (je ne parle pas ménage… 😏.)
+            </p>
+            <p>
+                Dans la totalité des jobs que j’ai effectué, il y avait toujours les mêmes tâches à répéter. Des tableaux excel en doublon.
+            </p>
+            <p>
+            J’ai la conviction qu’une application métier bien développée permet de ne pas répéter ces tâches et donc de gagner en productivité, fiabilité et en bien-être opérateur.
+            </p>
+            <p>
+                Si vous avez besoin d’une application métier dédiée, échanger sur ce sujet ou autre, n’hésitez pas à me contacter (rubrique <em>Me contacter</em>)
+            </p>
+            <p>
+            Bonne visite !!! 
+            </p>
         </div>
     );
 }
--- src/components/Before/index.js
+++ src/components/Before/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import './style.scss';
-
+import '../About/style.scss';
+import Emoji from 'react-emoji-render';
 
 const Before=() => {
     
     return (
-        <div className="before">
-            before
+        <div className="content">
+            <h1 className="content-before-title">
+                Avant d'être développeur 
+            </h1>
+            <p>
+                J’ai un BAC S options Physique / Chimie.<Emoji text=":)"/>
+            </p>
+            <p>            
+                J’ai toujours été plus attiré par les Sciences que par les Lettres. Je suis plus rationnel qu’émotionnel.
+            </p>
+            <p>
+                Après le bac, j’ai poursuivi avec un DUT Mesure Physique.
+            </p>
+            <hr></hr>
+            <p>
+                Comme la majorité des futurs étudiants, je ne savais pas quoi faire après le bac.
+            </p>
+            <p>
+            J’ai hésité avec un DUT en informatique mais à l’époque, le PC n’était économiquement pas accessible.
+            J’avais un CPC 464 (pour 464 octets, pas 464 Méga ou Giga). J’avais du mal à imaginer ce que je pourrais faire avec ce matériel. J’ai donc choisi une autre voie.
+            Je n’ai pas de regrets car j’ai rencontré des gens très compétents (d’autres un peu moins quand même) et j’ai eu la chance de participer à différents projets très motivants (détection de défaut par vision).
+            </p>
+            <hr></hr>
+            <p>
+                Après le DUT, j’ai travaillé dans différents domaines de l’industrie (fabriquant de matière, automobile, plasturgie pour l’automobile, plasturgie pour la pharmacie, étalonnage) mais aussi dans la comptabilité.
+            </p>
+            <p>
+                J’ai occupé des postes très différents (technicien d’essais, technicien qualité, métrologue, technicien développement, comptable, responsable de laboratoire….)
+            </p>
+            <hr></hr>
+            <p>
+                J’ai occupé beaucoup de postes car j’ai besoin de m’impliquer dans de nouveaux projets, faire des découvertes, apprendre.
+                Si vous pouvez m’offrir cela en tant qu’employeur, je suis prêt à m’engager à vos côtés pour relever de nouveaux challenges.
+            </p>
+
         </div>
     );
 }
